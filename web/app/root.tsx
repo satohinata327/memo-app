7ae458be8bd04bd5a505f6bdb734b92d6d6fffd1
Hinata Sato <satohinata327@gmail.com>
2026-08-06 23:58:42 +0900

    Feature/web (#9)
    * react-routerの設定
    
    * welcome/の削除
    
    * Hello World出るか確認

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -25,7 +25,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -41,6 +41,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+//現在のURLに対応する子ルートが表示される
 export default function App() {
   return <Outlet />;
 }
